206d7e135827738134500bed9048a4a63cde423f
Asif Nawaz <Asif.n@aimviz.com>
2018-03-29 12:23:32 +0500

    Designing Fixes

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -22,6 +22,18 @@
     <ul class="sidebar-menu" data-widget="tree">             
       <li class="header">PAGES</li>        
       <li><a href="{{route('users')}}"><i class="fa fa-user text-aqua"></i> <span>Users</span></a></li>
+      <li class="treeview">
+         <a href="#">
+           <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+           <span class="pull-right-container">
+             <i class="fa fa-angle-left pull-right"></i>
+           </span>
+         </a>
+         <ul class="treeview-menu" style="display: none;">
+           <li><a href="../../index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
+           <li><a href="../../index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+         </ul>
+       </li>
     </ul>
   </section>
   <!-- /.sidebar -->
