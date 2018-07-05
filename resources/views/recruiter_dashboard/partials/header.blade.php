285133a677be7cff595394b1a97da7c797decc68
unknown <Sheheryar.n@Soft-20-PC.aimviz.com>
2018-07-05 15:47:09 +0500

    Pull

--- resources/views/recruiter_dashboard/partials/header.blade.php
+++ resources/views/recruiter_dashboard/partials/header.blade.php
@@ -47,6 +47,57 @@
                            <label for="text" class="glyphicon glyphicon-search" rel="tooltip" title="text"></label>
                         </div>
                      </div>
+                     <div class="searchSuggetionCont ">
+                       <div class="resultClose pull-right"></div>
+                       <div>
+                         <ul id="hkb" class="hkb-searchresults">
+                           <li class="hkb-searchresults__article hkb-post-type-ht_kb format-standard">
+                             <a href="#" onclick="window.open(this.href, &quot;windowName&quot;, &quot;width=500, height=500, right=20, top=20, scrollbars, resizable&quot;);return false;">
+                               <span class="hkb-searchresults__title">Guide On How To Manage Reports</span>
+                               <span class="hkb-searchresults__excerpt">&nbsp; Codeground provides easy to interpret reports that aid recruiters in viewing, managing and maintaining crucial candidate test results. This...</span>
+                             </a>
+                           </li>
+                           <li class="hkb-searchresults__article hkb-post-type-ht_kb format-standard">
+                             <a href="#" onclick="window.open(this.href, &quot;windowName&quot;, &quot;width=500, height=500, right=20, top=20, scrollbars, resizable&quot;);return false;">
+                               <span class="hkb-searchresults__title">Guide On How To Manage Reports</span>
+                               <span class="hkb-searchresults__excerpt">&nbsp; Codeground provides easy to interpret reports that aid recruiters in viewing, managing and maintaining crucial candidate test results. This...</span>
+                             </a>
+                           </li>
+                           <li class="hkb-searchresults__article hkb-post-type-ht_kb format-standard">
+                             <a href="#" onclick="window.open(this.href, &quot;windowName&quot;, &quot;width=500, height=500, right=20, top=20, scrollbars, resizable&quot;);return false;">
+                               <span class="hkb-searchresults__title">Guide On How To Manage Reports</span>
+                               <span class="hkb-searchresults__excerpt">&nbsp; Codeground provides easy to interpret reports that aid recruiters in viewing, managing and maintaining crucial candidate test results. This...</span>
+                             </a>
+                           </li>
+                           <li class="hkb-searchresults__article hkb-post-type-ht_kb format-standard">
+                             <a href="#" onclick="window.open(this.href, &quot;windowName&quot;, &quot;width=500, height=500, right=20, top=20, scrollbars, resizable&quot;);return false;">
+                               <span class="hkb-searchresults__title">Guide On How To Manage Reports</span>
+                               <span class="hkb-searchresults__excerpt">&nbsp; Codeground provides easy to interpret reports that aid recruiters in viewing, managing and maintaining crucial candidate test results. This...</span>
+                             </a>
+                           </li>
+                           <li class="hkb-searchresults__article hkb-post-type-ht_kb format-standard">
+                             <a href="#" onclick="window.open(this.href, &quot;windowName&quot;, &quot;width=500, height=500, right=20, top=20, scrollbars, resizable&quot;);return false;">
+                               <span class="hkb-searchresults__title">Guide On How To Manage Reports</span>
+                               <span class="hkb-searchresults__excerpt">&nbsp; Codeground provides easy to interpret reports that aid recruiters in viewing, managing and maintaining crucial candidate test results. This...</span>
+                             </a>
+                           </li>
+                           <li class="hkb-searchresults__article hkb-post-type-ht_kb format-standard">
+                             <a href="#" onclick="window.open(this.href, &quot;windowName&quot;, &quot;width=500, height=500, right=20, top=20, scrollbars, resizable&quot;);return false;">
+                               <span class="hkb-searchresults__title">Guide On How To Manage Reports</span>
+                               <span class="hkb-searchresults__excerpt">&nbsp; Codeground provides easy to interpret reports that aid recruiters in viewing, managing and maintaining crucial candidate test results. This...</span>
+                             </a>
+                           </li>
+                           <li class="hkb-searchresults__showall">
+                             <a href="#" onclick="window.open(this.href, &quot;windowName&quot;, &quot;width=500, height=500, right=20, top=20, scrollbars, resizable&quot;);return false;">
+                               Show all results
+                             </a>
+                           </li>
+                           <li class="hkb-searchresults__noresults">
+                             <span>No Results</span>
+                           </li>
+                         </ul>
+                       </div>
+                     </div>
                   </li>
                </ul>
             </div>
