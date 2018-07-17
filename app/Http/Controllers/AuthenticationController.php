26de9332b4903ed55e71d07bf4b6536f10c5e3c6
Asif Nawaz <Asif.n@aimviz.com>
2018-07-17 18:11:29 +0500

    assign role working

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Events\UserProfile;
-use App\Events\UserCompanyProfile;
-use App\Profile;
+-
 use App\User;
 use Auth;
 class AuthenticationController extends Controller
